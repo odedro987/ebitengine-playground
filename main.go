405f4b60a42aba466702f58051fc4039cbaf179a
Oded Rosenblatt <odedro987@gmail.com>
2022-07-23 16:41:43 +0200

    Upgraded to ebiten v2

--- main.go
+++ main.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"log"
+	"strconv"
 
-	"github.com/hajimehoshi/ebiten"
+	_ "image/png"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 // Game implements ebiten.Game interface.
-type Game struct{}
+type Game struct{
+    time float64
+}
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
-func (g *Game) Update(screen *ebiten.Image) error {
+func (g *Game) Update() error {
     // Write your game's logical update.
+    g.time += 1 / 60.0
     return nil
 }
 
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-    // Write your game's rendering.
+    ebitenutil.DebugPrint(screen, strconv.FormatFloat(g.time, 'E', -1, 32))
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-    return 320, 240
+    return 640, 480
 }
 
 func main() {
     game := &Game{}
+
     // Specify the window size as you like. Here, a doubled size is specified.
     ebiten.SetWindowSize(640, 480)
     ebiten.SetWindowTitle("Your game's title")

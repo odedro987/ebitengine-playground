221565119f1c91da603ecf4a6b80481930d464cf
Oded Rosenblatt <odedro987@gmail.com>
2022-07-24 16:12:11 +0200

    Added GlxPoint helper funcs

--- gixel/math/point.go
+++ gixel/math/point.go
@@ -10,3 +10,31 @@ func NewPoint(x, y float64) *GxlPoint {
 		Y: y,
 	}
 }
+
+func (p *GxlPoint) Set(x, y float64) {
+	p.X, p.Y = x, y
+}
+
+func (p *GxlPoint) Copy(p2 *GxlPoint) {
+	p.X, p.Y = p2.X, p2.Y
+}
+
+func (p *GxlPoint) Mult(p2 *GxlPoint) GxlPoint {
+	return GxlPoint{X: p.X * p2.X, Y: p.Y * p2.Y}
+}
+
+func (p *GxlPoint) Div(p2 *GxlPoint) GxlPoint {
+	if p2.X == 0.0 || p2.Y == 0.0 {
+		panic("Zero division")
+	}
+
+	return GxlPoint{X: p.X / p2.X, Y: p.Y / p2.Y}
+}
+
+func (p *GxlPoint) Add(p2 *GxlPoint) GxlPoint {
+	return GxlPoint{X: p.X + p2.X, Y: p.Y + p2.Y}
+}
+
+func (p *GxlPoint) Sub(p2 *GxlPoint) GxlPoint {
+	return GxlPoint{X: p.X - p2.X, Y: p.Y - p2.Y}
+}

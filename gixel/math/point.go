e79c86c7eab41574dc870ca57ef01393e8cd47ca
Ron L <ronlifin@gmail.com>
2022-08-15 20:21:31 +0300

    Added util functions to GxlPoint

--- gixel/math/point.go
+++ gixel/math/point.go
@@ -1,10 +1,16 @@
 package math
 
-import "log"
+import (
+	"log"
+	"math"
+)
 
 type GxlPoint struct {
 	X, Y float64
 }
+
+const TO_RAD = math.Pi / 180
+const TO_DEG = 180 / math.Pi
 
 // NewPoint creates a new instance of GxlPoint with given coordinates.
 func NewPoint(x, y float64) *GxlPoint {
@@ -56,4 +62,24 @@ func (p *GxlPoint) Add(p2 *GxlPoint) GxlPoint {
 // Returns a new instance of a GxlPoint with the result.
 func (p *GxlPoint) Sub(p2 *GxlPoint) GxlPoint {
 	return GxlPoint{X: p.X - p2.X, Y: p.Y - p2.Y}
+}
+
+func (p *GxlPoint) SetRadians(rads float64) {
+	length := p.Length()
+	p.X = math.Cos(rads) * length
+	p.Y = math.Sin(rads) * length
+}
+
+func (p *GxlPoint) Length() float64 {
+	return math.Sqrt(p.X*p.X + p.Y*p.Y)
+}
+
+func (p *GxlPoint) PivotRadians(pivot *GxlPoint, radians float64) GxlPoint {
+	point1 := pivot.Sub(p)
+	point1.SetRadians(radians)
+	return GxlPoint{point1.X + pivot.X, point1.Y + pivot.Y}
+}
+
+func (p *GxlPoint) PivotDegrees(pivot *GxlPoint, degrees float64) GxlPoint {
+	return p.PivotRadians(pivot, degrees*TO_RAD)
 }
